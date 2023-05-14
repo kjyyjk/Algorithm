7575286ff1d39bd9cba27b30292c328a49457b24
kjyyjk <124787447+kjyyjk@users.noreply.github.com>
2023-05-14 22:04:17 +0900

    [Bronze II] Title: 상수, Time: 124 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -5,33 +5,25 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String a,b;
+        int a,b;
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        a = st.nextToken();
-        b = st.nextToken();
-
         StringBuilder sb = new StringBuilder();
 
-        if(a.charAt(2)>b.charAt(2))
+        sb.append(br.readLine());
+
+        StringTokenizer st = new StringTokenizer(sb.reverse().toString());
+
+        sb.delete(0,sb.length());
+
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+
+        if(a>b)
             sb.append(a);
-        else if(a.charAt(2)<b.charAt(2))
+        else
             sb.append(b);
-        else{
-            if(a.charAt(1)>b.charAt(1))
-                sb.append(a);
-            else if(a.charAt(1)<b.charAt(1))
-                sb.append(b);
-            else{
-                if(a.charAt(0)>b.charAt(0))
-                    sb.append(a);
-                else
-                    sb.append(b);
-            }
-        }
-        System.out.println(sb.reverse());
+
+        System.out.println(sb);
     }
 }
