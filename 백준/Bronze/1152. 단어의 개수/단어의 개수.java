659eb94ee502853c422e6f7b0717d9e02b5ea348
kjyyjk <124787447+kjyyjk@users.noreply.github.com>
2023-05-14 21:43:17 +0900

    [Bronze II] Title: 단어의 개수, Time: 152 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -1,14 +1,35 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        StringBuilder sb = new StringBuilder();
+         int count = 0;
+         int pre_str = 32; //첫 공백에 증가하는 것을 막기 위해 시작 시 이전 입력을 공백으로
+         int str;
 
-        System.out.println(sb.append(st.countTokens()));
+         while(true){
+             str = System.in.read();
+             
+             //공백 입력 시
+             if(str == 32){
+                 //이전 입력이 공백이 아니면 증가
+                 if(pre_str != 32){
+                     count++;
+                 }
+             }
+             //개행, 즉 엔터 입력 시
+             else if(str == 10){
+                 //이전 입력 공백 아니면
+                 if(pre_str != 32){
+                     count++;
+                 }
+                 break;
+             }
+
+             //반복 한번 끝나면 입력받은 문자가 이전문자가 된다
+             pre_str = str;
+         }
+
+        System.out.println(count);
     }
 }
+
