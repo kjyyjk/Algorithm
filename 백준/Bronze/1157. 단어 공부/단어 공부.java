e325cc38b7457f5bb7e0219270fd99e5825c33f3
kjyyjk <124787447+kjyyjk@users.noreply.github.com>
2023-05-17 13:49:17 +0900

    [Bronze I] Title: 단어 공부, Time: 284 ms, Memory: 20076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.java
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -1,12 +1,14 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         int[] arr;
         int i;
         int ind = 0;
-        int count = 0;
         int max = 0;
+        char c= '?';
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
@@ -26,22 +28,15 @@ public class Main {
         for(i=0; i<arr.length; i++){
             if(arr[i]>max) {
                 max = arr[i];
-                ind = i;
+                c = (char)(i+65);
             }
-        }
-
-        for(i=0; i<arr.length; i++){
-            if(arr[i]==max)
-                count++;
+            else if(arr[i]==max)
+                c = '?';
         }
 
         StringBuilder sb = new StringBuilder();
 
-        if(count>1)
-            sb.append("?");
-        else {
-            sb.append((char)(ind+65));
-        }
+        sb.append(c);
 
         System.out.println(sb);
     }
