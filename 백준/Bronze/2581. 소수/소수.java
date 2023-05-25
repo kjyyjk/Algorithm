2a0d6e8f3a2f305a1bfa80de20f99ea992383d5d
kjyyjk <124787447+kjyyjk@users.noreply.github.com>
2023-05-25 13:54:53 +0900

    [Bronze II] Title: 소수, Time: 312 ms, Memory: 14568 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.java
@@ -0,0 +1,37 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        int min = n;
+        int hap = 0;
+        int count;
+
+        for(int i = m; i<n+1; i++){
+            count = 0;
+            for(int j = 1; j<i+1; j++){
+                if(i%j==0)
+                    count++;
+            }
+
+            if(count==2){
+                hap += i;
+                if(min>i)
+                    min = i;
+            }
+        }
+        StringBuilder sb = new StringBuilder();
+        if(hap==0)
+            sb.append(-1);
+        else
+            sb.append(hap).append('\n').append(min);
+        
+        System.out.println(sb);
+    }
+}
