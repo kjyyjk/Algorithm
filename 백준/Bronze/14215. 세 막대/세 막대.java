5e7b3d5d332ee216e8a8bd4ffce3524c04012e49
kjyyjk <124787447+kjyyjk@users.noreply.github.com>
2023-05-29 21:08:10 +0900

    [Bronze III] Title: 세 막대, Time: 124 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/14215. 세 막대/세 막대.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringBuilder sb = new StringBuilder();
+        br.close();
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+
+        int max = Math.max(Math.max(a,b),c);
+        int result = a+b+c-max;
+
+        if(max<result)
+            sb.append(a+b+c);
+        else
+            sb.append(result + result - 1);
+
+        System.out.println(sb);
+    }
+}
