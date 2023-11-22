fc8a07a94bfec7771d7ed907c736bec52b2747a7
kjyyjk <rlawnsdud920@gmail.com>
2023-11-22 11:14:50 +0900

    백준_2018_수들의합5

---
+++ src/백준/BJ_2018.java
@@ -0,0 +1,36 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_2018 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        int start_pointer = 1;
+        int end_pointer = 1;
+        int sum = 1;
+        int cnt = 1;
+
+        while(end_pointer!=n){
+
+            if(n > sum) {
+                end_pointer++;
+                sum += end_pointer;
+            } else if (n < sum) {
+                sum -= start_pointer;
+                start_pointer++;
+            } else {
+                end_pointer++;
+                sum += end_pointer;
+                cnt++;
+            }
+        }
+
+        System.out.println(new StringBuilder().append(cnt).append('\n'));
+    }
+
+}
