2d3b130b41dadc7ef79778794802038e6e98049a
kjyyjk <rlawnsdud920@gmail.com>
2023-12-01 16:01:52 +0900

    백준_2164_카드2

---
+++ src/백준/BJ_2164.java
@@ -0,0 +1,34 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BJ_2164 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int i, temp;
+        int n = Integer.parseInt(br.readLine());
+
+        Queue<Integer> queue = new LinkedList<>();
+
+        for(i=1; i<n+1; i++) {
+            queue.offer(i);
+        }
+
+        while(queue.size()>1) {
+            queue.poll();
+            temp = queue.poll();
+            queue.offer(temp);
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        System.out.println(sb.append(queue.poll()));
+    }
+}
