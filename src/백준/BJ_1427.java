a0b463154855c4f9361cf28ee93e82507683dd9a
kjyyjk <rlawnsdud920@gmail.com>
2024-01-13 16:36:19 +0900

    백준_1427_소트인사이드

---
+++ src/백준/BJ_1427.java
@@ -0,0 +1,42 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_1427 {
+
+    public static void main(String[] args) throws IOException {
+
+        int i, j, maxInd, temp;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String n = br.readLine();
+        int[] arr = new int[n.length()];
+
+        for(i=0; i<n.length(); i++) {
+            arr[i] = Integer.parseInt(String.valueOf(n.charAt(i)));
+        }
+
+        for(i=0; i<arr.length-1; i++) {
+            maxInd = i;
+            for(j=i; j<arr.length; j++) {
+                if(arr[j] > arr[maxInd]) {
+                    maxInd = j;
+                }
+            }
+
+            temp = arr[i];
+            arr[i] = arr[maxInd];
+            arr[maxInd] = temp;
+        }
+
+        for(i=0; i<arr.length; i++) {
+            sb.append(arr[i]);
+        }
+
+        System.out.println(sb);
+
+    }
+}
