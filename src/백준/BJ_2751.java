0b1acda4c0f9577f1a4776e9cafd113cbe9a6758
kjyyjk <rlawnsdud920@gmail.com>
2024-01-17 12:30:32 +0900

    백준_2751_수정렬하기2
    다른 코드를 참고하여
    메모리 와 속도 개선

---
+++ src/백준/BJ_2751.java
@@ -0,0 +1,69 @@
+package 백준;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BJ_2751 {
+    static int[] arr, tempArr;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        arr = new int[n];
+        tempArr = new int[n];
+
+        for(int i=0; i<n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        mergeSort(0, n-1);
+
+        for(int i=0; i<n; i++) {
+            sb.append(arr[i]).append('\n');
+        }
+
+        System.out.println(sb);
+    }
+
+    static void mergeSort(int s, int e) {
+        if(e-s < 1) {
+            return;
+        }
+
+        int m = (e+s)/2;
+
+        mergeSort(s,m);
+        mergeSort(m+1,e);
+
+        int i, j;
+
+        for(i=s; i<e+1; i++) {
+            tempArr[i] = arr[i];
+        }
+
+        int arrInd = s;
+        i = s;
+        j = m+1;
+
+        while(i<m+1 && j<e+1) {
+            if(tempArr[i] > tempArr[j]) {
+                arr[arrInd++] = tempArr[j++];
+            } else if (tempArr[i] < tempArr[j]) {
+                arr[arrInd++] = tempArr[i++];
+            }
+        }
+
+        while(i<m+1) {
+            arr[arrInd++] = tempArr[i++];
+        }
+
+        while(j<e+1) {
+            arr[arrInd++] = tempArr[j++];
+        }
+
+    }
+}
