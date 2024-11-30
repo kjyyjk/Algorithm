9bddfffaa3d470f4302c897d8994187d8f5b81a0
kjyyjk <rlawnsdud920@gmail.com>
2024-11-30 20:42:36 +0900

    백준_17298_오큰수_stack

--- src/백준/BJ_17298_오큰수구하기.java
+++ src/백준/BJ_17298_오큰수구하기.java
@@ -3,48 +3,43 @@ package 백준;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
+/**
+ * 시간 복잡도로 인해 이중 for문 무식하게는 안된다.
+ * 짝짓기 -> 스택
+ * 오큰수가 나오지 않으면 스택에 보관하고 있다가
+ * 오큰수가 나오면 스택에서 꺼내고 오큰수를 저장한다.
+ */
 public class BJ_17298_오큰수구하기 {
-
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int n = Integer.parseInt(br.readLine());
+        int[] ret = new int[n+4];
 
-        int[] inputArr = new int[n];
-        int[] resultArr = new int[n];
+        Arrays.fill(ret, -1);
+
+        int[] input = new int[n+4];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            input[i] = Integer.parseInt(st.nextToken());
+        }
 
         Stack<Integer> stack = new Stack<>();
-
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
-        for(int i=0; i<n; i++) {
-            inputArr[i] = Integer.parseInt(st.nextToken());
-        }
-
-        stack.push(0);
-
-        for(int i=0; i<n-1; i++) {
-
-            while(!stack.isEmpty() && inputArr[stack.peek()] < inputArr[i+1]) {
-                resultArr[stack.pop()] = inputArr[i+1];
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && input[stack.peek()] < input[i]) {
+                ret[stack.peek()] = input[i];
+                stack.pop();
             }
-
-            stack.push(i+1);
+            stack.push(i);
         }
 
-        while(!stack.isEmpty()) {
-            resultArr[stack.pop()] = -1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(ret[i]).append(' ');
         }
-
-        for(int i=0; i<n; i++) {
-            sb.append(resultArr[i]).append(' ');
-        }
-
         System.out.println(sb);
     }
 }
