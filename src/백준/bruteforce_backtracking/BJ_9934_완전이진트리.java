35d57e299a6e66c1c9ca5364cd5a4b52105a6173
kjyyjk <rlawnsdud920@gmail.com>
2024-12-26 13:15:10 +0900

    백준_9934_완전이진트리
    - 레벨 별 트리 출력에 최적화된 코드

--- src/백준/bruteforce_backtracking/BJ_9934_완전이진트리.java
+++ src/백준/bruteforce_backtracking/BJ_9934_완전이진트리.java
@@ -3,11 +3,20 @@ package 백준.bruteforce_backtracking;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
+/**
+ * 문제에서는 완전 이진트리에 inorder 순회를 한다.
+ * 따라서 주어진 구간에서 가운데 위치한 노드가 해당 구간의 루트가 될 것이다.
+ * 가운데, 즉 서브 루트를 기준으로 왼쪽 구간은 왼쪽 자식 트리, 오른쪽 구간은 오른쪽 자식 트리.
+ *
+ * 재귀함수 go()를 통해 전체 구간을 시작으로 탐색 시작.
+ */
 public class BJ_9934_완전이진트리 {
     static int[] arr;
-    static int[] result;
+    static List<Integer>[] result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -15,7 +24,10 @@ public class BJ_9934_완전이진트리 {
         int end = (int) (Math.pow(2, k) - 1);
 
         arr = new int[end];
-        result = new int[end + 1];
+        result = new List[k + 1];
+        for (int i = 1; i < k + 1; i++) {
+            result[i] = new ArrayList<>();
+        }
 
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         for (int i = 0; i < end; i++) {
@@ -25,21 +37,24 @@ public class BJ_9934_완전이진트리 {
         go(0, end - 1, 1);
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < k; i++) {
-            int temp = (int) Math.pow(2, i);
-            for (int j = temp; j < temp * 2; j++) {
-                sb.append(result[j]).append(' ');
+        for (int i = 1; i < k + 1; i++) {
+            for (int j = 0; j < result[i].size(); j++) {
+                sb.append(result[i].get(j)).append(' ');
             }
             sb.append('\n');
         }
         System.out.println(sb);
     }
 
-    static void go(int a, int b, int ind) {
-        int root = (a + b) / 2;
-        result[ind] = arr[root];
-        if (a == b) return;
-        go(a, root - 1, ind * 2);
-        go(root + 1, b, ind * 2 + 1);
+    static void go(int s, int e, int level) {
+        if (s > e) return;
+        if (s == e) {
+            result[level].add(arr[s]);
+            return;
+        }
+        int root = (s + e) / 2;
+        result[level].add(arr[root]);
+        go(s, root - 1, level + 1);
+        go(root + 1, e, level + 1);
     }
 }
