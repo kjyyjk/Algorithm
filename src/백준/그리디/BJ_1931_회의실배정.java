206441fbadf5650316e7e148a2da83c26ae566eb
kjyyjk <rlawnsdud920@gmail.com>
2025-09-17 20:21:49 +0900

    백준_1931_회의실배정
    - 회의실 많이 배정한다 -> 각 회의가 겹치지 않으며 최대한 빨리 끝나야한다.
    
    - 종료 시간 기준 오름차순 정렬(같은 경우 시작 시간 기준 오름차순 정렬)
    
    - (5, 10) 이후 (7, 12)가 나오면 겹치니까 가차 없이 버린다. 만약
    (5, 10)을 버리고 (7, 12)를 골라서 회의 수가 늘어난다는 가정이 성립될라면.
    (5, 7) 과 같은 회의가 있었어야함.
    하지만 만약 있었다면 풀이 원칙에 의해 이미 (5, 10)이 아니라 (5, 7)이어야함.

--- src/백준/그리디/BJ_1931_회의실배정.java
+++ src/백준/그리디/BJ_1931_회의실배정.java
@@ -1,63 +1,52 @@
 package 백준.그리디;
 
-import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
+
 import java.util.StringTokenizer;
+import java.util.Arrays;
 
+/**
+ * 길이 n인 arr을 정렬 + 전체 순회
+ * O(nlogn) + O(n) = O(nlogn)
+ *
+ * n<= 100,000
+ *
+ * O(13 * 00000)
+ */
 public class BJ_1931_회의실배정 {
-
     public static void main(String[] args) throws IOException {
-
-        StringTokenizer st;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
 
-        Queue<Node> pQ = new PriorityQueue<>(new Comparator<Node>() {
-            @Override
-            public int compare(Node o1, Node o2) {
-                if(o1.e > o2.e) {
-                    return 1;
-                } else if (o1.e < o2.e) {
-                    return -1;
-                } else {
-                    return o1.s - o2.s;
-                }
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i] = new int[]{Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())};
+        }
+
+        Arrays.sort(arr, (a, b) ->  {
+            if (a[1] == b[1]) { // 종료 시간이 같을 경우 시작 시간 기준 오름차순 정렬
+                return a[0] - b[0];
+            } else {
+                return a[1] - b[1]; // 종료 시간 기준 오름차순 정렬
             }
         });
 
-        for (int i = 0; i<n; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            pQ.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
-        }
+        int count = 0;
+        int prevFinishTime = 0;
 
-        Node nowNode;
-        int cnt = 0;
-        int end = 0;
-
-        while(!pQ.isEmpty()) {
-            nowNode = pQ.poll();
-            if(nowNode.s >= end) {
-                cnt++;
-                end = nowNode.e;
+        // arr은 이미 빨리 끝나는 순으로 정렬된 상태.
+        for (int i = 0; i < n; i++) {
+            if (prevFinishTime <= arr[i][0]) { // 이전 확정 회의와 겹치지 않는 경우만
+                count++;
+                prevFinishTime = arr[i][1];
             }
         }
 
-        System.out.println(new StringBuilder().append(cnt));
-    }
-
-    static class Node{
-        int s;
-        int e;
-
-        public Node(int s, int e) {
-            this.s = s;
-            this.e = e;
-        }
+        System.out.println(count);
     }
 
 }
